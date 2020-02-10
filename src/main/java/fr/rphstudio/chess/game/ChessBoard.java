670c5ebbfa8525252edca2f725498933871efea9
Maxence Lavenu <maxence.lavenu@outlook.fr>
2020-02-10 15:23:49 +0100

    Dynamic Board Generation complete

--- src/main/java/fr/rphstudio/chess/game/ChessBoard.java
+++ src/main/java/fr/rphstudio/chess/game/ChessBoard.java
@@ -9,21 +9,21 @@ public class ChessBoard {
     public ChessBoard() {
 
         this.board = new Piece[IChess.BOARD_WIDTH][IChess.BOARD_HEIGHT];
-        for (int line = 0; line <= 7; line += 7) {
-            this.board[0][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
-            this.board[1][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
-            this.board[2][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
-            this.board[3][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_QUEEN);
-            this.board[4][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KING);
-            this.board[5][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
-            this.board[6][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
-            this.board[7][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
+        for (int line = IChess.BOARD_POS_Y_BLACK_PIECES; line <= IChess.BOARD_POS_Y_WHITE_PIECES; line += IChess.BOARD_POS_Y_WHITE_PIECES) {
+            this.board[IChess.BOARD_POS_X_QUEENSIDE_ROOK][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
+            this.board[IChess.BOARD_POS_X_QUEENSIDE_KNIGHT][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
+            this.board[IChess.BOARD_POS_X_QUEENSIDE_BISHOP][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
+            this.board[IChess.BOARD_POS_X_QUEEN][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_QUEEN);
+            this.board[IChess.BOARD_POS_X_KING][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KING);
+            this.board[IChess.BOARD_POS_X_KINGSIDE_BISHOP][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_BISHOP);
+            this.board[IChess.BOARD_POS_X_KINGSIDE_KNIGHT][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
+            this.board[IChess.BOARD_POS_X_KINGSIDE_ROOK][line] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
         }
 
 
         for (int pawnX = 0; pawnX < IChess.BOARD_WIDTH; pawnX++) {
-            this.board[pawnX][1] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
-            this.board[pawnX][6] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
+            this.board[pawnX][IChess.BOARD_POS_Y_BLACK_PAWNS] = new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_PAWN);
+            this.board[pawnX][IChess.BOARD_POS_Y_WHITE_PAWNS] = new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_PAWN);
         }
     }
 
