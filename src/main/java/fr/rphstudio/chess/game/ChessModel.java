3bb6615f25bd2632cef2f5ca29e0500486e57d1f
Furradev <brian.lecarpentier@edu.itescia.fr>
2020-02-10 10:11:20 +0100

    Story 1 done
    we can launch the game

---
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -0,0 +1,74 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.EmptyCellException;
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.OutOfBoardException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChessModel implements IChess{
+
+
+    private ChessModel(){
+
+    }
+
+    /** Instance unique pré-initialisée */
+    private static ChessModel INSTANCE = new ChessModel();
+
+
+    public static IChess getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void reinit() {
+
+    }
+
+    @Override
+    public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return ChessType.TYP_PAWN;
+    }
+
+    @Override
+    public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return ChessColor.CLR_WHITE;
+    }
+
+    @Override
+    public int getNbRemainingPieces(ChessColor color) {
+        return 0;
+    }
+
+    @Override
+    public List<ChessPosition> getPieceMoves(ChessPosition p) {
+        return new ArrayList<ChessPosition>();
+    }
+
+    @Override
+    public void movePiece(ChessPosition p0, ChessPosition p1) {
+
+    }
+
+    @Override
+    public ChessKingState getKingState(ChessColor color) {
+        return null;
+    }
+
+    @Override
+    public List<ChessType> getRemovedPieces(ChessColor color) {
+        return new ArrayList<ChessType>();
+    }
+
+    @Override
+    public boolean undoLastMove() {
+        return false;
+    }
+
+    @Override
+    public long getPlayerDuration(ChessColor color, boolean isPlaying) {
+        return 0;
+    }
+}
