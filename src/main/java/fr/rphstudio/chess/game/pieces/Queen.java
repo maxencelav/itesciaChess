c195d6c579f0bb504790f3a2c62b4dfc83b46d6a
Furradev <brian.lecarpentier@edu.itescia.fr>
2020-02-11 11:29:17 +0100

    Add queen

--- src/main/java/fr/rphstudio/chess/game/pieces/Queen.java
+++ src/main/java/fr/rphstudio/chess/game/pieces/Queen.java
@@ -12,8 +12,44 @@ public class Queen implements IMove {
     public List<IChess.ChessPosition> getPossibleMoves(IChess.ChessPosition p, ChessBoard board) {
         List<IChess.ChessPosition> possibleMoves = new ArrayList<>();
 
-        possibleMoves.add((new IChess.ChessPosition(3,6)));
+        List<Integer> posX = new ArrayList<Integer>();
+        List<Integer> posY = new ArrayList<Integer>();
 
-        return  possibleMoves;
+        // coordinates generator
+        for (int i = 0; i < IChess.BOARD_WIDTH; i++) {
+
+            posX.add(p.x + i);
+            posX.add(p.x - i);
+            posX.add(p.x);
+            posX.add(p.x);
+
+            posY.add(p.y);
+            posY.add(p.y);
+            posY.add(p.y + i);
+            posY.add(p.y - i);
+
+            posX.add(p.x + i);
+            posX.add(p.x + i);
+            posX.add(p.x - i);
+            posX.add(p.x - i);
+
+            posY.add(p.y + i);
+            posY.add(p.y - i);
+            posY.add(p.y + i);
+            posY.add(p.y - i);
+        }
+
+        for (int i = 0; i < posX.size(); i++) {
+            IChess.ChessPosition dest = new IChess.ChessPosition(posX.get(i), posY.get(i));
+            if ((posX.get(i) >= 0 && posX.get(i) < 8 && posY.get(i) >= 0 && posY.get(i) < 8) && board.getPiece(dest) == null) {
+
+                possibleMoves.add(dest);
+
+                //si couleur adverse on arrête au x de la case
+                // si couleur alliée on arrête au x avant la case
+
+            }
+        }
+        return possibleMoves;
     }
 }
