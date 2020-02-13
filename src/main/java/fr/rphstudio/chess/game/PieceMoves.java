49c8726564a74d49ddf6d3ae737c0ba5618efdd4
Maxence Lavenu <maxence.lavenu@outlook.fr>
2020-02-13 22:59:50 +0100

    JAVADOC : PieceMoves

--- src/main/java/fr/rphstudio/chess/game/PieceMoves.java
+++ src/main/java/fr/rphstudio/chess/game/PieceMoves.java
@@ -8,45 +8,59 @@ public class PieceMoves {
     private ArrayList<Move> moves;
 
 
+    /**
+     * Creates a PieceMoves list and adds the board, round time, and player color given as the very first step
+     *
+     * @param savedBoard  Board of the move
+     * @param roundTime   Time the move took
+     * @param playerColor Color of the current player
+     */
     public PieceMoves(ChessBoard savedBoard, long roundTime, IChess.ChessColor playerColor) {
-
         moves = new ArrayList<>();
         this.addMove(savedBoard, roundTime, playerColor);
-
     }
 
-    public void reset(long roundTime, IChess.ChessColor playerColor) {
-
-        moves = new ArrayList<>();
-
-
-    }
-
+    /**
+     * Adds a move to the PieceMoves list
+     *
+     * @param savedBoard  Board of the move
+     * @param roundTime   Time the move took
+     * @param playerColor Color of the current player
+     */
     public void addMove(ChessBoard savedBoard, long roundTime, IChess.ChessColor playerColor) {
         moves.add(new Move(savedBoard, roundTime, playerColor));
     }
 
 
+    /**
+     * Gets the very last move played in the PieceMove
+     *
+     * @return last Move added to the PieceMove
+     */
     public Move getLastMove() {
         try {
-
             return moves.get(moves.size() - 1);
-
         } catch (Exception e) {
-
         }
         return null;
     }
 
+    /**
+     * Removes the very last move played in the PieceMove
+     */
     public void removeLastMove() {
         try {
             moves.remove(moves.size() - 1);
         } catch (Exception e) {
-
         }
 
     }
 
+    /**
+     * Gets the number of turns (elements) in the PieceMove list
+     *
+     * @return number of turns played
+     */
     public int getNumberOfTurns() {
         return moves.size();
     }
