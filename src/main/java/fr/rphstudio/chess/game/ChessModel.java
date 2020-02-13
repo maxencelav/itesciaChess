d23bd18473f623c39810dd5431d3306b8c2ce3ff
Maxence Lavenu <maxence.lavenu@outlook.fr>
2020-02-13 21:38:08 +0100

    COMMENTS + JAVADOC : ChessModel

--- src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -16,13 +16,15 @@ public class ChessModel implements IChess {
 
 
     private ChessModel() {
-        chessBoard = new ChessBoard();//create a chess board
-        timer = new TimeManager();//create a timer
-        pieceMoves = new PieceMoves(chessBoard, 0, ChessColor.CLR_WHITE);//piece'movement
+        chessBoard = new ChessBoard(); // create a chess board
+        timer = new TimeManager(); // create a timer
+        pieceMoves = new PieceMoves(chessBoard, 0, ChessColor.CLR_WHITE);
+        // generate a piece movement history with the first movement being the basic board
     }
 
     /**
-     * Instance unique pré-initialisée
+     * Unique pre-initialized instance
+     * (Instance unique pré-initialisée)
      */
     private static ChessModel INSTANCE = new ChessModel();
 
@@ -32,17 +34,20 @@ public class ChessModel implements IChess {
     }
 
     /**
-     * Reset the chess board, the timer and pices's movement
+     * Reset the chess board, the timer and pieces's movement
      */
     @Override
     public void reinit() {
-        this.chessBoard = new ChessBoard();
-        this.timer.resetTimer();
+        this.chessBoard = new ChessBoard(); // generate a new chessboard
+        this.timer.resetTimer(); // reset the timer
         this.pieceMoves = new PieceMoves(chessBoard, 0, ChessColor.CLR_WHITE);
+        // generate a piece movement history with the first movement being the (new) basic board
+
     }
 
     /**
-     * Get the piece type
+     * Get a piece's type
+     *
      * @param p x/y position on the board where we want to get the piece type.
      * @return the piece type
      * @throws EmptyCellException
@@ -60,7 +65,8 @@ public class ChessModel implements IChess {
     }
 
     /**
-     * Get the piece color
+     * Get a piece's color
+     *
      * @param p x/y position on the board where we want to get the piece color.
      * @return the piece color
      * @throws EmptyCellException
@@ -79,6 +85,7 @@ public class ChessModel implements IChess {
 
     /**
      * Get the number of pieces remaining on the board
+     *
      * @param color the requested color of the pieces to count.
      * @return the number of piece remaining for the given color
      */
@@ -88,35 +95,34 @@ public class ChessModel implements IChess {
         int colorCounter = 0;
 
         for (int y = 0; y < BOARD_HEIGHT; y++) {
-            for (int x = 0; x < BOARD_WIDTH; x++) {
+            for (int x = 0; x < BOARD_WIDTH; x++) { // for each piece on the board
                 try {
                     if (chessBoard.getPiece(new ChessPosition(x, y)).getPieceColor() == color) {
-                        //incrementation of the counter of remaining pieces for the given color
+                        // if piece is of the color wanted
+                        // incrementation of the counter of remaining pieces for the given color
                         colorCounter++;
                     }
                 } catch (NullPointerException e) {
                 }
-
             }
         }
         return colorCounter;
     }
 
     /**
+     * Gets the possible positions for a piece's movement
      *
      * @param p requested piece position.
-     * @return
+     * @return a list containing ChessPosition elements, that are the possible moves
      */
     @Override
     public List<ChessPosition> getPieceMoves(ChessPosition p) {
-
-
         Piece currentPiece = chessBoard.getPiece(p);
 
         List<ChessPosition> moveList = chessBoard.getPiece(p).getMoves(p, chessBoard);
         List<ChessPosition> safeMoveList = new ArrayList<>();
 
-        // get all moves and only return ones that wont kill the king
+        // get all moves and only return ones that won't kill the king
         for (ChessPosition positionToCheck : moveList) {
             ChessBoard clonedBoard = chessBoard.clone();
             clonedBoard.movePiece(p, positionToCheck);
@@ -129,22 +135,30 @@ public class ChessModel implements IChess {
         return safeMoveList;
     }
 
+    /**
+     * Moves a piece on the board. Adds the removed piece to the list of the removed piece types.
+     *
+     * @param p0 source position on the board.
+     * @param p1 destination position on the board.
+     */
     @Override
     public void movePiece(ChessPosition p0, ChessPosition p1) {
-        ChessBoard boardBeforeMove = chessBoard.clone();
-        Piece removedPiece = chessBoard.movePiece(p0, p1);
-        long turnTime = timer.changeTurn(chessBoard.getPiece(p1).getPieceColor());
-        pieceMoves.addMove(boardBeforeMove.clone(), turnTime, chessBoard.getPiece(p1).getPieceColor());
+        ChessBoard boardBeforeMove = chessBoard.clone(); // clones the board
+        Piece removedPiece = chessBoard.movePiece(p0, p1); // gets the removed piece
+        long turnTime = timer.changeTurn(chessBoard.getPiece(p1).getPieceColor()); // gets the time of the move in milliseconds
+        pieceMoves.addMove(boardBeforeMove.clone(), turnTime, chessBoard.getPiece(p1).getPieceColor()); // adds the move to the move history
 
         if (removedPiece != null) { // if a piece was removed
             chessBoard.addRemovedPiece(removedPiece.getPieceType(), removedPiece.getPieceColor());
+            // add the removed piece to the list of the removed pieces
         }
     }
 
     /**
+     * Gets the status of the King for a given color
      *
      * @param color the requested king color.
-     * @return
+     * @return KING_THREATEN if the King is in danger, or KING_SAFE if not
      */
     @Override
     public ChessKingState getKingState(ChessColor color) {
@@ -153,9 +167,10 @@ public class ChessModel implements IChess {
     }
 
     /**
+     * Get a list of the removed piece types for display
      *
      * @param color color of the removed pieces
-     * @return
+     * @return a list containing all of the removed piece types for the color given
      */
     @Override
     public List<ChessType> getRemovedPieces(ChessColor color) {
@@ -163,30 +178,31 @@ public class ChessModel implements IChess {
     }
 
     /**
+     * Goes back one move in time
      *
-     * @return
+     * @return a boolean to indicate if a move has been removed, or not if it is the first move on the board
      */
     @Override
     public boolean undoLastMove() {
-        boolean aaa = false;
-        Move lastMove = pieceMoves.getLastMove();
+        boolean hasMoveBeenReverted = false;
+        Move lastMove = pieceMoves.getLastMove(); // gets the last Move object added to the list
 
-        //if (lastMove != null) {
-        if (pieceMoves.getNumberOfTurns() > 1) {
+        if (pieceMoves.getNumberOfTurns() > 1) { // if it's NOT the first turn
 
-            this.timer.decreaseTime(lastMove.playerColor,lastMove.getTurnTime());
-            this.chessBoard = lastMove.getSavedBoard();
-            pieceMoves.removeLastMove();
-            aaa = true;
+            this.timer.decreaseTime(lastMove.playerColor, lastMove.getTurnTime()); // remove time from the player
+            this.chessBoard = lastMove.getSavedBoard(); // get the board from the previous move
+            pieceMoves.removeLastMove(); // remove the move from the list now that it's back in the game
+            hasMoveBeenReverted = true;
         }
-        return aaa; // a changer pour que si jamais lastMove = null ça return false
+        return hasMoveBeenReverted;
     }
 
     /**
-     * 
-     * @param color The color of the player from whom we want to get the current duration.
+     * Gets the current time of the player
+     *
+     * @param color     The color of the player from whom we want to get the current duration.
      * @param isPlaying Indicates if the player color is the one currently playing.
-     * @return
+     * @return time of the player's turn(s) in milliseconds
      */
     @Override
     public long getPlayerDuration(ChessColor color, boolean isPlaying) {
